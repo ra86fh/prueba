3b925eeaad1a5c06351a865d8922aec1d3fbf34a
Raul <raul@kosday.com>
2022-08-09 10:40:16 +0200

    Create auto-updating app

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+require('update-electron-app')({
+    updateInterval: '5 minutes',
+    logger: require('electron-log')
+})
+
+const loadMainWindow = () => {
+    const mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    mainWindow.loadFile(path.join(__dirname, "index.html"));
+}
+
+app.on("ready", loadMainWindow);
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        loadMainWindow();
+    }
+});
